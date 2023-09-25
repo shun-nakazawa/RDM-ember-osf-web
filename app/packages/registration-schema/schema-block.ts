eee48b9246dd24436db4f0ae35cae7e6f6090199
shun-nakazawa <shun.nakazawa@voice-research.com>
2023-09-26 02:37:37 +0900

    support required_if and remove unnecessary code

--- app/packages/registration-schema/schema-block.ts
+++ app/packages/registration-schema/schema-block.ts
@@ -26,14 +26,7 @@ export type SchemaBlockType =
     'e-rad-researcher-name-en-input' |
     'e-rad-bunnya-input' |
     'file-metadata-input' |
-    'date-input' |
-    'file-capacity-input' |
-    'file-creators-input' |
-    'file-url-input' |
-    'file-institution-ja-input' |
-    'file-institution-en-input' |
-    'file-institution-id-input' |
-    'file-data-number-input';
+    'date-input';
 
 export interface SchemaBlock {
     id?: string;
@@ -44,6 +37,7 @@ export interface SchemaBlock {
     helpText?: string;
     exampleText?: string;
     required?: boolean;
+    requiredIf?: string;
     default?: boolean;
     index?: number;
     pattern?: string;
